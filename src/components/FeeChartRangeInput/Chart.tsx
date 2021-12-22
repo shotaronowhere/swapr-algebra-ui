366b32766857a1e1d2a3cfe2e1e80f70cbcd902c
lilchizh <xlilchizhx@gmail.com>
2021-12-22 17:32:23 +0300

    chart month fix

--- src/components/FeeChartRangeInput/Chart.tsx
+++ src/components/FeeChartRangeInput/Chart.tsx
@@ -459,7 +459,7 @@ export default function Chart({
               ? `${date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()}:${
                   date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
                 }:${date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()}`
-              : `${date.getDate()}/${date.getMonth() - 1}/${date.getFullYear()}`
+              : `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
           )
           Focus.attr('transform', `translate(${xScale(_chartData[i].timestamp)},${y(_chartData[i]?.value)})`)
         }
