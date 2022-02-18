998c9627f87d3aa05d6854741f96d66966a82ff7
lilchizh <xlilchizhx@gmail.com>
2022-02-18 18:11:09 +0300

    duplo fix

---
+++ src/components/Web3Status/Sock.tsx
@@ -0,0 +1,9 @@
+import { t } from "@lingui/macro";
+
+export function Sock() {
+    return (
+        <span role="img" aria-label={t`has socks emoji`} style={{ marginTop: -4, marginBottom: -4 }}>
+            🧦
+        </span>
+    );
+}
---
+++ src/components/Web3Status/StatusIcon.tsx
@@ -0,0 +1,10 @@
+import { AbstractConnector } from "@web3-react/abstract-connector";
+import { injected } from "../../connectors";
+import Identicon from "../Identicon";
+
+export function StatusIcon({ connector }: { connector: AbstractConnector }) {
+    if (connector === injected) {
+        return <Identicon />;
+    }
+    return null;
+}
