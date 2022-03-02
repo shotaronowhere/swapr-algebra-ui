d202d566ba1dc7e3913acca6a0bd1f6280c52d78
lilchizh <xlilchizhx@gmail.com>
2022-03-02 19:56:22 +0300

    fix

--- src/pages/FarmingEventsPage/index.tsx
+++ src/pages/FarmingEventsPage/index.tsx
@@ -1,81 +1,74 @@
-import { useEffect, useMemo, useState } from 'react'
-import { Frown } from 'react-feather'
-import { StakerEventCard } from '../../components/StakerEventCard'
-import { StakeModal } from '../../components/StakeModal'
-import { FarmingType } from '../../models/enums'
-import Modal from '../../components/Modal'
-import { EmptyMock, EventsCards, EventsCardsRow, PageWrapper } from './styled'
-import Loader from '../../components/Loader'
+import { useEffect, useMemo, useState } from "react";
+import { Frown } from "react-feather";
+import { StakerEventCard } from "../../components/StakerEventCard";
+import { StakeModal } from "../../components/StakeModal";
+import { FarmingType } from "../../models/enums";
+import Modal from "../../components/Modal";
+import { EmptyMock, EventsCards, EventsCardsRow, PageWrapper } from "./styled";
+import Loader from "../../components/Loader";
 
-export function FarmingEventsPage({
-    data,
-    now,
-    refreshing,
-    fetchHandler
-}: {
-    data: { currentEvents: any[]; futureEvents: any[] } | null
-    now: number
-    refreshing: boolean
-    fetchHandler: () => any
-}) {
-    const [modalForPool, setModalForPool] = useState(null)
+export function FarmingEventsPage({ data, now, refreshing, fetchHandler }: { data: { currentEvents: any[]; futureEvents: any[] } | null; now: number; refreshing: boolean; fetchHandler: () => any }) {
+    const [modalForPool, setModalForPool] = useState(null);
 
-    const formattedData = useMemo(() => {
-        if (!data || typeof data === 'string') return []
+    // const formattedData = useMemo(() => {
+    //     if (!data || typeof data === 'string') return []
 
-        return [...data?.futureEvents, ...data?.currentEvents]
-    }, [data])
+    //     return [...data?.futureEvents, ...data?.currentEvents]
+    // }, [data])
+
+    const formattedData: any[] = [];
 
     useEffect(() => {
-        fetchHandler()
-    }, [])
+        fetchHandler();
+    }, []);
 
     return (
         <PageWrapper>
-            <Modal isOpen={Boolean(modalForPool)} onHide={() => setModalForPool(null)}
-                   onDismiss={() => console.log()}>
+            <Modal isOpen={Boolean(modalForPool)} onHide={() => setModalForPool(null)} onDismiss={() => console.log()}>
                 {modalForPool && (
                     <>
-                        <StakeModal
-                            event={modalForPool}
-                            closeHandler={() => setModalForPool(null)}
-                            farmingType={FarmingType.FINITE}
-                        />
+                        <StakeModal event={modalForPool} closeHandler={() => setModalForPool(null)} farmingType={FarmingType.FINITE} />
                     </>
                 )}
             </Modal>
             <EventsCards>
                 {refreshing ? (
                     <EmptyMock>
-                        <Loader stroke={'white'} size={'20px'} />
+                        <Loader stroke={"white"} size={"20px"} />
                     </EmptyMock>
-                ) : formattedData.length !== 0 ?
+                ) : formattedData.length !== 0 ? (
                     <EventsCardsRow>
-                        {formattedData.map(
-                            (event, j) => {
+                        {formattedData.map((event, j) => {
+                            const isStarted = event.startTime <= Math.round(Date.now() / 1000);
+                            const isEnded = event.endTime <= Math.round(Date.now() / 1000);
 
-                                const isStarted = event.startTime <= Math.round(Date.now() / 1000)
-                                const isEnded = event.endTime <= Math.round(Date.now() / 1000)
+                            if (isEnded) return;
 
-                                if (isEnded) return
+                            const active = isStarted && !isEnded;
 
-                                const active = isStarted && !isEnded
-
-                                return <StakerEventCard refreshing={refreshing} active={active}
-                                                        key={j} now={now} event={event}
-                                                        stakeHandler={() => {
-                                                            setModalForPool(event)
-                                                        }} />
-                            }
-                        )}
+                            return (
+                                <StakerEventCard
+                                    refreshing={refreshing}
+                                    active={active}
+                                    key={j}
+                                    now={now}
+                                    event={event}
+                                    stakeHandler={() => {
+                                        setModalForPool(event);
+                                    }}
+                                />
+                            );
+                        })}
                     </EventsCardsRow>
-                    : formattedData && formattedData.length === 0 ? (
-                        <EmptyMock>
-                            <div>No limit farms</div>
-                            <Frown size={35} stroke={'white'} />
-                        </EmptyMock>
-                    ) : <EmptyMock />}
+                ) : formattedData && formattedData.length === 0 ? (
+                    <EmptyMock>
+                        <div>No limit farms</div>
+                        <Frown size={35} stroke={"white"} />
+                    </EmptyMock>
+                ) : (
+                    <EmptyMock />
+                )}
             </EventsCards>
         </PageWrapper>
-    )
+    );
 }
