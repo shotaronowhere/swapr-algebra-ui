077ee04723a1f6d41ccbb5c733d0fff1b146deba
lilchizh <xlilchizhx@gmail.com>
2022-03-21 19:57:08 +0300

    algebra is default output token

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -122,11 +122,14 @@ export function useDerivedSwapInfo(): {
         recipient
     } = useSwapState()
 
-    const inputCurrency = useCurrency(inputCurrencyId)
-    const outputCurrency = useCurrency(outputCurrencyId)
+
+    const inputCurrency = useCurrency(inputCurrencyId || '0x0169eC1f8f639B32Eec6D923e24C2A2ff45B9DD6')
+    const outputCurrency = useCurrency(outputCurrencyId || '0x0169eC1f8f639B32Eec6D923e24C2A2ff45B9DD6')
 
     const recipientLookup = useENS(recipient ?? undefined)
     const to: string | null = (recipient === null ? account : recipientLookup.address) ?? null
+
+    console.log('CURRENCIES', inputCurrency, outputCurrency)
 
     const relevantTokenBalances = useCurrencyBalances(account ?? undefined, [
         inputCurrency ?? undefined,
