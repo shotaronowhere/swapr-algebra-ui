cfb34037ba47602d9276df04cf126a804bad7200
MirYasa <gurevich.miron14@gmail.com>
2022-04-13 10:52:01 +0300

    fix farm history

--- src/pages/EventsHistory/TokenReward.tsx
+++ src/pages/EventsHistory/TokenReward.tsx
@@ -5,18 +5,19 @@ import { SupportedChainId } from '../../constants/chains'
 import { WrappedCurrency } from '../../models/types'
 
 interface TokenRewardProps {
-    token: { address: string; symbol: string }
-    reward: number
+    token: { address: string; symbol: string } | null
+    reward: number | null
 }
 
 export default function TokenReward({ token, reward }: TokenRewardProps) {
     return (
         <div className={'f f-ac'}>
-            <CurrencyLogo size='2rem' currency={new Token(SupportedChainId.POLYGON, token.address, 18, token.symbol) as WrappedCurrency}
-            />
+            {token ?
+                <CurrencyLogo size='2rem' currency={new Token(SupportedChainId.POLYGON, token.address, 18, token.symbol) as WrappedCurrency} />
+            : <span className={'ml-05'}>-</span>}
             <div className={'ml-05'}>
                 <div>{reward}</div>
-                <div>{token.symbol} </div>
+                <div>{token?.symbol} </div>
             </div>
         </div>
     )
