453699c571b397d2c62d66641f35e34f5445a1d2
lilchizh <xlilchizhx@gmail.com>
2022-06-03 12:06:28 +0300

    autoscroll

--- src/pages/NewAddLiquidity/routing/router-guards.tsx
+++ src/pages/NewAddLiquidity/routing/router-guards.tsx
@@ -1,9 +1,14 @@
 import Loader from "components/Loader";
+import { useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
 import { useCurrentStep } from "state/mint/v3/hooks";
 
 export function RouterGuard({ Component, allowance, redirect, ...rest }: { Component: any; allowance: any; redirect: string; [x: string]: any }) {
     const currentStep = useCurrentStep();
+
+    useEffect(() => {
+        window.scrollTo({ top: 0 });
+    }, []);
 
     return allowance ? (
         <Route
