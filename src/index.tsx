90c0d74bd48e90b8c80fe5299275c841f8a8c1fb
lilchizh <xlilchizhx@gmail.com>
2022-07-08 18:59:42 +0300

    subraph

--- src/index.tsx
+++ src/index.tsx
@@ -35,7 +35,7 @@ if (_window.ethereum) {
 }
 
 const client = new ApolloClient({
-    uri: "https://api.thegraph.com/subgraphs/name/iliaazhel/algebra",
+    uri: "https://api.thegraph.com/subgraphs/name/iliaazhel/info-test",
     cache: new InMemoryCache(),
 });
 
