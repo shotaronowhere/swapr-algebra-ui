f3f68b24ccfc5dd91e89ac53904b0d65298188e8
Berteotti <berteotti2@gmail.com>
2023-11-24 11:10:04 +0000

    fix: provide english language exclusively

--- src/constants/locales.ts
+++ src/constants/locales.ts
@@ -1,14 +1,8 @@
-export const SUPPORTED_LOCALES = [
-    'en-US',
-    'ru-RU',
-    'es-ES'
-] as const
-export type SupportedLocale = typeof SUPPORTED_LOCALES[number]
+export const SUPPORTED_LOCALES = ["en-US"] as const;
+export type SupportedLocale = typeof SUPPORTED_LOCALES[number];
 
-export const DEFAULT_LOCALE: SupportedLocale = 'en-US'
+export const DEFAULT_LOCALE: SupportedLocale = "en-US";
 
 export const LOCALE_LABEL: { [locale in SupportedLocale]: string } = {
-    'en-US': 'English',
-    'es-ES': 'Español',
-    'ru-RU': 'Русский',
-}
+    "en-US": "English",
+};
